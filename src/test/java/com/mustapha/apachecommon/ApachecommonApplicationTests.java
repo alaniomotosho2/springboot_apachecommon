7f8242c2e5b9d0c94cf2b1e98fb3f1c985ffed64
mustapha <mustapha.omotosho@flexisaf.com>
2022-06-02 15:36:54 +0100

    first commit

---
+++ src/test/java/com/mustapha/apachecommon/ApachecommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustapha.apachecommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApachecommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
