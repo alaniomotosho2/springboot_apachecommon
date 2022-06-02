8a71e64ea9a6b88d3e7dd96171e825231f5be145
mustapha <mustapha.omotosho@flexisaf.com>
2022-06-02 15:50:03 +0100

    commonIO

--- src/test/java/com/mustapha/apachecommon/ApachecommonApplicationTests.java
+++ src/test/java/com/mustapha/apachecommon/ApachecommonApplicationTests.java
@@ -1,13 +1,31 @@
 package com.mustapha.apachecommon;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ApachecommonApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void commonIo() throws IOException {
+		String initialString = "Hello World!";
+		try(InputStream inputStream = new ByteArrayInputStream(initialString.getBytes());
+			ByteArrayOutputStream targetStream = new ByteArrayOutputStream()){
+			IOUtils.copy(inputStream,targetStream);
+			assertEquals(initialString, new String(targetStream.toByteArray()));
+		}
 	}
 
 }
